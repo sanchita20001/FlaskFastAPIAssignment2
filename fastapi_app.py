788498026a7f17ffaa3fa7909ff48fc6ea4ca262
sanchita20001 <sanchitrouth09@gmail.com>
2024-09-25 14:42:49 +0530

    Add files via upload

---
+++ fastapi_app.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/")
+def read_root():
+  return{"message":"welcome to the FastAPI app"}
+if __name__=='__main__':
+  import uvicorn  
+  uvicorn.run(app,host='0.0.0.0',port=86000)
